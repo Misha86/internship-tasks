f29a0ad10823622e9390d4550b8c4b1ab6530783
Misha86 <mpolishchuk86@gmail.com>
2022-04-21 00:23:24 +0300

    split tasks

---
+++ tasks_86_330_tests_87_226_559/task_86a.py
@@ -0,0 +1,8 @@
+# task 86a
+def count_digits(number):
+    return len(str(number))
+
+
+if __name__ == "__main__":
+    numb = 13456
+    print(count_digits(numb))
--- tasks_86_330_tests_87_226_559/task_86b.py
+++ tasks_86_330_tests_87_226_559/task_86b.py
@@ -1,8 +1,3 @@
-# task 86a
-def count_digits(number):
-    return len(str(number))
-
-
 # task 86b
 def sum_digits(number):
     return sum([int(num) for num in str(number)])
@@ -10,5 +5,4 @@ def sum_digits(number):
 
 if __name__ == "__main__":
     numb = 13456
-    print(count_digits(numb))
     print(sum_digits(numb))
