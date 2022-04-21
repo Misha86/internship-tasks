9d6ee230e9c2554defc15f24db1016061272d8f4
Misha86 <mpolishchuk86@gmail.com>
2022-04-21 19:52:44 +0300

    Added doc string for test 87

--- tasks_86_330_tests_87_226_559/test_87.py
+++ tasks_86_330_tests_87_226_559/test_87.py
@@ -7,31 +7,42 @@ import unittest
 
 # cut & sum the digit from tail of target number a given number of times
 def get_sum_of_lasts_few(target: int, tail_size: int) -> int:
+    """
+    Get last digits sum in the target
+    :param target: int
+    :param tail_size: int
+    :return: int
+    """
     if tail_size > len(str(target)):
         raise ValueError("Number tail size > number!")
     if target == 0:
         raise ValueError("Zero is not natural number!")
     if tail_size == 1:
         return target % 10
-    else:
-        tail_size -= 1
-        return target % 10 + get_sum_of_lasts_few(target // 10, tail_size)
+    tail_size -= 1
+    return target % 10 + get_sum_of_lasts_few(target // 10, tail_size)
 
 
 class TestSumOfLastsFew(unittest.TestCase):
 
+    """Tests for function get_sum_of_lasts_few."""
+
     def test_target_arg_zero(self):
+        """Test checks when number is zero."""
         with self.assertRaises(ValueError):
             get_sum_of_lasts_few(0, 1)
 
     def test_tail_size(self):
+        """Test checks when tail size is more than number."""
         with self.assertRaises(ValueError):
             get_sum_of_lasts_few(1, 5)
 
     def test_tail_size_one(self):
+        """Test checks when tail size equal 1."""
         self.assertEqual(get_sum_of_lasts_few(112, 1), 2)
 
     def test_sum_of_lasts_few(self):
+        """Test checks the get_sum_of_lasts_few function correct result."""
         self.assertEqual(get_sum_of_lasts_few(112, 2), 3)
 
 
