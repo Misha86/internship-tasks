91a5db0d671a9163e74869eebda33d6c8f530f4f
Misha86 <mpolishchuk86@gmail.com>
2022-04-21 18:33:04 +0300

    Added docstrings to task 86b

--- tasks_86_330_tests_87_226_559/task_86b.py
+++ tasks_86_330_tests_87_226_559/task_86b.py
@@ -1,8 +1,16 @@
+"""This module provides function to get digits sum in the number."""
+
+
 # task 86b
-def sum_digits(number):
+def sum_digits(number: int) -> int:
+    """
+    Get digits sum in the number
+    :param number: int
+    :return: int
+    """
     return sum([int(num) for num in str(number)])
 
 
 if __name__ == "__main__":
-    numb = 13456
-    print(sum_digits(numb))
+    NUMBER = 13456
+    print(sum_digits(NUMBER))
