d8a9d2251272da3aecbfa042b4d724ba4b13f47e
Misha86 <mpolishchuk86@gmail.com>
2022-04-22 05:22:16 +0300

    Changed task 330

--- tasks_86_330_tests_87_226_559/task_330.py
+++ tasks_86_330_tests_87_226_559/task_330.py
@@ -2,22 +2,50 @@
 import time
 
 
+def sieve_of_eratosthenes(number):
+    """Create a boolean array "prime[0..n]" and initialize
+     all entries it as true. A value in prime[i] will
+     finally be false if i is Not a prime, else true."""
+    prime = [True] * (number + 1)
+    position = 2
+    while position * position <= number:
+
+        # If prime[position] is not changed, then it is a prime
+        if prime[position] is True:
+
+            # Update all multiples of position
+            for i in range(position ** 2, number + 1, position):
+                prime[i] = False
+        position += 1
+    prime[0] = False
+    prime[1] = False
+    if prime[number]:
+        return True
+    return False
+
+
 def perfect_numbers(number: int) -> list[int]:
     """
     Get perfect numbers in range(1, number)
     :param number: int
     :return: list[int]
     """
-    numbers = []
-    for num in range(0, number, 2):
-        dividers = [div for div in range(1, int(num / 2) + 1) if num % div == 0]
-        if sum(dividers) == num:
-            numbers.append(num)
-    return numbers[1:]
+
+    result = [6, ]
+
+    for index in range(1, 100, 2):
+        mermen_candidate = pow(2, index) - 1
+        sieve = sieve_of_eratosthenes(mermen_candidate)
+        if sieve:
+            value = pow(2, index - 1) * (pow(2, index) - 1)
+            if value >= number:
+                break
+            result.append(value)
+    return result
 
 
 if __name__ == "__main__":
-    NUMBER = 10000
+    NUMBER = 1000000000000
     start = time.time()
     print(perfect_numbers(NUMBER))
     print(time.time() - start)
