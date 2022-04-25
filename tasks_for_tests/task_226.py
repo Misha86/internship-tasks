76f543809fa3f79cccc6c7e7b81cff128ab4d163
Misha86 <mpolishchuk86@gmail.com>
2022-04-25 10:41:50 +0300

    changed main

---
+++ tasks_for_tests/task_226.py
@@ -0,0 +1,20 @@
+"""This module consists of task_226."""
+
+
+# return all natural common multiples less than first_arg * second_arg
+def task_226(first_arg: int, second_arg: int) -> list[int]:
+    """
+    Get natural common multiples
+    :param first_arg: int
+    :param second_arg: int
+    :return: list[int]
+    """
+    limit = first_arg * second_arg
+    suspect = second_arg
+    result = []
+    while suspect < limit:
+        if suspect % first_arg == 0:
+            result.append(suspect)
+        suspect += second_arg
+
+    return result
