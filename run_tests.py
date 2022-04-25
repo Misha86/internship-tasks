a928e3121d0a90792fad952c86e2a17dc938a702
Misha86 <mpolishchuk86@gmail.com>
2022-04-25 11:15:42 +0300

    add run_tests for tests in not current dir

---
+++ run_tests.py
@@ -0,0 +1,28 @@
+""" Group tests for tasks. """
+import os
+import unittest
+
+
+# def suite():
+#     """ Add all test from current file to TestSuite. """
+#     loader = unittest.TestLoader()
+#     tests = loader.loadTestsFromName('__main__')
+#     suite_tests = unittest.TestSuite()
+#     suite_tests.addTests(tests)
+#     return suite_tests
+
+
+def suite(tests_dir='tests', pattern='tests_task_*.py'):
+    """ Add all test from current tests`s folder to TestSuite. """
+    this_dir = os.path.join(os.path.dirname(__file__), tests_dir)
+    loader = unittest.TestLoader()
+    suite_tests = unittest.TestSuite()
+    package_tests = loader.discover(start_dir=this_dir, pattern=pattern)
+    suite_tests.addTests(package_tests)
+    return suite_tests
+
+
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite())
+
